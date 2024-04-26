037f80ecfbceb4362982a18df7949c2c775c6b69
unknown <hoon81@gmail.com>
2024-04-26 12:43:19 -0400

    Add styling to books (card div)

--- script.js
+++ script.js
@@ -27,17 +27,17 @@ function displayAllBooks() {
         title.innerText = myLibrary[i].title;
         card.appendChild(title);
 
-        const author = document.createElement('h5');
+        const author = document.createElement('h4');
         author.classList.add('author');
         author.innerText = myLibrary[i].author;
         card.appendChild(author);
 
-        const pages = document.createElement('h5');
+        const pages = document.createElement('h6');
         pages.classList.add('pages');
         pages.innerText = "Pages: " + myLibrary[i].pages;
         card.appendChild(pages);
 
-        const read = document.createElement('h5');
+        const read = document.createElement('h6');
         read.classList.add('read');
         read.innerText = "Read: " + myLibrary[i].read;
         card.appendChild(read);
@@ -45,13 +45,26 @@ function displayAllBooks() {
 }
 
 const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkein', 295, false);
-const theHobbit2 = new Book('The Hobbit2', 'J.R.R. Tolkein', 295, false);
-const theHobbit3 = new Book('The Hobbit3', 'J.R.R. Tolkein', 295, false);
-const theHobbit4 = new Book('The Hobbit4', 'J.R.R. Tolkein', 295, false);
-const theHobbit5 = new Book('The Hobbit5', 'J.R.R. Tolkein', 295, false);
+const theHobbit2 = new Book('The Hobbit 2', 'J.R.R. Tolkein', 295, false);
+const theHobbit3 = new Book('The Hobbit 3', 'J.R.R. Tolkein', 295, false);
+const theHobbit4 = new Book('The Hobbit 4', 'J.R.R. Tolkein', 295, false);
+const theHobbit5 = new Book('The Hobbit 5', 'J.R.R. Tolkein', 295, false);
+const theHobbit6 = new Book('The Hobbit 6', 'J.R.R. Tolkein', 295, false);
+const theHobbit7 = new Book('The Hobbit 7', 'J.R.R. Tolkein', 295, false);
+const theHobbit8 = new Book('The Hobbit 8', 'J.R.R. Tolkein', 295, false);
+const theHobbit9 = new Book('The Hobbit 9', 'J.R.R. Tolkein', 295, false);
+const theHobbit10 = new Book('The Hobbit 10', 'J.R.R. Tolkein', 295, false);
+
 
 addBookToLibrary(theHobbit);
 addBookToLibrary(theHobbit2);
 addBookToLibrary(theHobbit3);
 addBookToLibrary(theHobbit4);
 addBookToLibrary(theHobbit5);
+addBookToLibrary(theHobbit6);
+addBookToLibrary(theHobbit7);
+addBookToLibrary(theHobbit8);
+addBookToLibrary(theHobbit9);
+addBookToLibrary(theHobbit10);
+
+displayAllBooks();
